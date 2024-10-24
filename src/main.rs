00565760ad754382001b9d2b7d6ea500ea8f3cf6
rezafikkri <fikkri.reza@gmail.com>
2024-10-24 07:23:24 +0700

    feat: immutable and mutable variable

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,20 @@ fn main() {
 
 #[test]
 fn test_hello() {
-  println!("This is hello world!");
+    println!("This is hello world!");
+}
+
+#[test]
+fn variable() {
+    let name = "Reza Sariful Fikri";
+    println!("My name is {name}");
+}
+
+#[test]
+fn variable_nutable() {
+    let mut name = "Reza Sariful Fikri";
+    println!("My name is {name}");
+
+    name = "Adelina Damayanti";
+    println!("My name now is {name}");
 }
